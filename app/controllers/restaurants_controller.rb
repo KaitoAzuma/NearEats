a7b358a8f0e761b1fa86e954abdd2815b628d945
KaitoAzuma <s225002@matsu.shimane-u.ac.jp>
2025-01-19 02:39:48 +0900

    検索条件入力画面と検索結果画面のデザインを作成, 位置情報取得の処理を微修正

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -17,10 +17,6 @@ class RestaurantsController < ApplicationController
     if lat.present? && lng.present?
       api_client = HotpepperApiClient.new()
       @restaurants = api_client.search_restaurants(lat, lng, range, count, options, name)
-
-      if @restaurants.nil? || @restaurants[:results][:shop].empty?
-        flash[:alert] = "レストラン情報が見つかりませんでした。"
-      end
     else
       @restaurants = nil
       flash[:alert] = "現在地情報が不足しています。緯度と経度を指定してください。"
